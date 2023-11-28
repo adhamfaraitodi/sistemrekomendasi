fd5c9d427f983e52cf42db1d62728b92b74a7fdb
Adham Faraitodi <109128115+adhamfaraitodi@users.noreply.github.com>
2023-11-28 22:20:07 +0700

    second commit

--- detail.php
+++ detail.php
@@ -9,10 +9,10 @@ $database = new Nette\Database\Connection($dsn, $user, $password);
 $id = $_GET['id'];
 $hotel = get_hotel_detail($id, $database);
 
-$result = $database->query('SELECT hotel_id, hotel_desc, address FROM hotel');
+$result = $database->query('SELECT id, Abstract, Title FROM jurnal');
 $data = [];
 foreach($result as $row){
-    $data[$row->hotel_id] = pre_process($row->hotel_desc.' '.$row->address);
+    $data[$row->id] = pre_process($row->Abstract.' '.$row->Title);
 }
 
 $cbrs = new Cbrs();
@@ -37,7 +37,7 @@ function pre_process($str){
 }
 
 function get_hotel_detail($id, $db){
-    $rs = $db->fetch('SELECT * FROM hotel Where hotel_id = '.$id);
+    $rs = $db->fetch('SELECT * FROM jurnal Where id = '.$id);
     return $rs;
 }
 
@@ -52,32 +52,27 @@ function get_hotel_detail($id, $db){
     <body>
         <div class="container theme-showcase">
             <div class="jumbotron">
-                <h1>Daftar Hotel di Surabaya</h1>
+                <h1>Daftar Jurnal dengan Tema Renewable Energy</h1>
                 <p>Contoh implementasi Sistem rekomendasi berbasis kontent menggunakan metode TF-IDF dan Cosine Similarity</p>
             </div>
             
             <div class="row">
-                <div class="col-md-2">
-                    <img src="https://via.placeholder.com/150" />
-                </div>
                 <div class="col-md-10">
-                    <h2><span class="label label-primary"><?php echo $hotel->hotel_name?></span></h2>
-                    <p><strong>Address:</strong> <?php echo $hotel->address?></p>
-                    <p><strong>Description:</strong> <?php echo $hotel->hotel_desc?></p>
-                    <p><strong>Price/Night:</strong> Rp <?php echo number_format($hotel->price_per_night)?></p>
+                    <h2><span class="label label-primary"><?php echo $hotel->Title?></span></h2>
+                    <p><strong>Abstract:</strong> <?php echo $hotel->Abstract?></p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <h3>Rekomendasi Hotel yang sesuai</h3>
+                    <h3>Rekomendasi Jurnal yang sesuai</h3>
                     <ol>
                         <?php $i=0;?>
                         <?php foreach($r as $k => $row):?>
                             <?php if($i==$n) break;?>
                             <?php if($row==1) continue;?>
                             <?php $h = get_hotel_detail($k, $database);?>
-                            <li><a href="detail.php?id=<?php echo $h->hotel_id ?>">
-                                <?php echo $h->hotel_name ?></a> (<?php echo $row?>)
+                            <li><a href="detail.php?id=<?php echo $h->id ?>">
+                                <?php echo $h->Title ?></a> (<?php echo $row?>)
                             </li>
                             <?php $i++ ?>
                         <?php endforeach ?>    
